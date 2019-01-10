5bfff4e7812468c86a6ae904f5a7f03a7e94b3d4
demetrios711 <demetri.sakellaropoulos@gmail.com>
2019-01-10 13:56:48 -0800

    Add Methods

---
+++ W1D5/map.rb
@@ -0,0 +1,20 @@
+  class Map
+    def initialize
+      # create ivar to store stack here!
+    end
+
+    def set(key,value)
+      # adds an element to the stack
+    end
+
+    def get(key)
+      # removes one element from the stack
+    end
+
+    def delete(key)
+      # returns, but doesn't remove, the top element in the stack
+    end
+    def show
+
+    end 
+  end
